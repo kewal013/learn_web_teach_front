839b7f0f28fc6569e420b60dffa28b724f92fab4
kewal <kewal013@gmail.com>
2018-03-30 10:42:58 +0530

    login signup directive

---
+++ app/scripts/directives/login.directive.js
@@ -0,0 +1,8 @@
+ecom.directive('login-page', function() {
+    return {
+        restrict: 'E',
+        // templateUrl: '../../templates/login_page.html'
+        templates: '<h1>kk</h1>'
+            // controller: footerController
+    }
+})
